4ee4d1855d0abd9a34c0cbafb72bb7bbff0c75e7
Lewis Bailey <55699401+Viserren@users.noreply.github.com>
2023-10-16 15:47:46 +0100

    Inventory
    Added
    - Basic inventory
    - Started adding save for inventory

--- Circuit B/Assets/Scripts/Data Persistance/Data/GameData.cs
+++ Circuit B/Assets/Scripts/Data Persistance/Data/GameData.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameData
 {
     public int deathCount;
+    public List<InventoryItem> inventory;
 
     /* 
      * The values defined in this constructor will be the default values
@@ -14,5 +15,6 @@ public class GameData
     public GameData()
     {
         this.deathCount = 0;
+        this.inventory = new List<InventoryItem>();
     }
 }
---
+++ Circuit B/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class InventoryManager : MonoBehaviour, Id
+{
+    public SO_Item test;
+    private List<InventoryItem> items = new List<InventoryItem>();
+
+    public void AddItemToIntentory(SO_Item itemToAdd, int amountToAdd = 1)
+    {
+        try
+        {
+            InventoryItem currentItem = items.Single(item => item.item == itemToAdd);
+            currentItem.IncreaseAmount(amountToAdd);
+            Debug.Log($"{currentItem.name}, x{currentItem.quantity}");
+        }
+        catch (System.InvalidOperationException)
+        {
+            items.Add(new InventoryItem(itemToAdd, amountToAdd));
+            Debug.Log($"New Item Found: {itemToAdd.name}");
+        }
+    }
+
+    public void RemoveItemFromIntentory(SO_Item itemToRemove, int amountToRemove = 1)
+    {
+        try
+        {
+            InventoryItem currentItem = items.Single(item => item.item == itemToRemove);
+            currentItem.DecreaseAmount(amountToRemove);
+            Debug.Log($"{currentItem.name}, x{currentItem.quantity}");
+        }
+        catch (System.InvalidOperationException)
+        {
+            Debug.LogError("How you removing a item that isnt there???");
+        }
+    }
+
+    public void ClearInventory()
+    {
+        items.Clear();
+    }
+}
+
+[System.Serializable]
+public class InventoryItem
+{
+    public string name { get; private set; }
+    public SO_Item item { get; private set; }
+    public int quantity { get; private set; }
+
+    public InventoryItem(SO_Item item, int quantity)
+    {
+        name = item.name;
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public void IncreaseAmount(int amountToAdd)
+    {
+        quantity += amountToAdd;
+    }
+
+    public void DecreaseAmount(int amountToRemove)
+    {
+        quantity -= amountToRemove;
+        if (quantity <= 0)
+        {
+            quantity = 0;
+        }
+    }
+}
+
+[CustomEditor(typeof(InventoryManager))]
+public class InventoryManagerEditor : Editor
+{
+    SerializedProperty inventoryItems;
+    private void OnEnable()
+    {
+        inventoryItems = serializedObject.FindProperty("items");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        InventoryManager inventoryManager = (InventoryManager)target;
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Add Item"))
+        {
+            inventoryManager.AddItemToIntentory(inventoryManager.test);
+        }
+
+        if (GUILayout.Button("Remove Item"))
+        {
+            inventoryManager.RemoveItemFromIntentory(inventoryManager.test);
+        }
+
+        if (GUILayout.Button("Clear List"))
+        {
+            inventoryManager.ClearInventory();
+        }
+    }
+}
