fda490d46fdb4ba442dede5fd94d077e76341229
Lewis Bailey <55699401+Viserren@users.noreply.github.com>
2024-01-23 17:08:15 +0000

    Quality Control
    Added
    - Quality Settings

---
+++ Circuit B/Assets/Scripts/Settings/QualityController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class QualityController : MonoBehaviour
+{
+    public const string CURRENTQUALITY_KEY = "CurrentQualityKey";
+    int _quality;
+    [SerializeField] TMP_Dropdown _qualityDropDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey(CURRENTQUALITY_KEY))
+        {
+            SetFirstLoadQualityLevel();
+        }
+        LoadQualitySetting();
+    }
+
+    public void SwitchQualityLevel(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+    }
+
+    void ChangeAssetProperties()
+    {
+
+    }
+    public void SaveQualitySetting()
+    {
+        PlayerPrefs.SetInt(CURRENTQUALITY_KEY, _qualityDropDown.value);
+    }
+
+    void LoadQualitySetting()
+    {
+        _quality = PlayerPrefs.GetInt(CURRENTQUALITY_KEY, QualitySettings.GetQualityLevel());
+
+        _qualityDropDown.value = _quality;
+    }
+
+    void SetFirstLoadQualityLevel()
+    {
+        switch (SystemInfo.graphicsMemorySize)
+        {
+            case <= 2048:
+                QualitySettings.SetQualityLevel(1);
+
+                break;
+            case <= 4048:
+                QualitySettings.SetQualityLevel(2);
+                break;
+            default:
+                QualitySettings.SetQualityLevel(0);
+                break;
+        }
+        int temp = QualitySettings.GetQualityLevel();
+        PlayerPrefs.SetInt(CURRENTQUALITY_KEY, temp);
+    }
+}
