d77d30f245940cf8e6f71bb29d61dadcdc237947
Lewis Bailey <55699401+Viserren@users.noreply.github.com>
2024-03-13 17:43:16 +0000

    Camera pictures

--- Circuit B/Assets/Scripts/CameraCapture.cs
+++ Circuit B/Assets/Scripts/CameraCapture.cs
@@ -4,6 +4,8 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 public class CameraCapture : MonoBehaviour
@@ -27,26 +29,44 @@ public class CameraCapture : MonoBehaviour
 
     public void CamCapture()
     {
-        //mainCamera = Camera.main;
-        //mainCamera.clearFlags = CameraClearFlags.Depth;
+        flags = TextureCreationFlags.DontInitializePixels;
+        RenderTexture currentRT = new RenderTexture(_captureWidth, _captureHeight, 32, GraphicsFormat.B8G8R8A8_SRGB);
+        currentRT.depthStencilFormat = GraphicsFormat.D24_UNorm_S8_UInt;
 
-        RenderTexture currentRT = new RenderTexture(_captureWidth, _captureHeight, 32, GraphicsFormat.R8G8B8A8_UNorm);
         mainCamera.targetTexture = currentRT;
-        RenderTexture.active = mainCamera.targetTexture;
+        RenderTexture.active = currentRT;
 
         mainCamera.Render();
 
-        Texture2D Image = new Texture2D(_captureWidth, _captureHeight, TextureFormat.ARGB32, false);
+        Texture2D Image = new Texture2D(_captureWidth, _captureHeight, TextureFormat.RGBA32, false, true, true);
+
         Image.ReadPixels(new Rect(0, 0, _captureWidth, _captureHeight), 0, 0);
         Image.Apply();
 
-        //RenderTexture.active = null;
-        //mainCamera.targetTexture = null;
+        RenderTexture.active = null;
+        mainCamera.targetTexture = null;
+
+        Color32[] colors = Image.GetPixels32();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            //Debug.Log(colors[i].g);
+            if (colors[i].r > 0 || colors[i].g > 0 || colors[i].b > 0)
+            {
+                colors[i].a = 255;
+            }
+            else
+            {
+                colors[i].a = 0;
+            }
+        }
+        Image.SetPixels32(colors);
+        Image.Apply();
 
         var Bytes = Image.EncodeToPNG();
-        DestroyImmediate(Image);
 
+        DestroyImmediate(Image);
         File.WriteAllBytes($"{Application.dataPath}/Backgrounds/{_fileName} {_fileCounter}.png", Bytes);
+
         _fileCounter++;
     }
 }
